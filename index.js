af1998771942bd0b03c6453706e8c0f612320502
rafaapcode <rafaap2003@gmail.com>
2022-04-08 19:59:31 -0300

    ChallengeResolved

--- index.js
+++ index.js
@@ -50,7 +50,75 @@ app.post("/users", (req, res) => {
 });
 
 app.get("/todos", checkExistsUserAccount, (req, res) => {
-    const {user} = req;
+    const { user } = req;
 
     return res.json(user.todos);
-})
+});
+
+app.post("/todos", checkExistsUserAccount, (req, res) => {
+    const { user } = req;
+    const { title, deadline } = req.body;
+
+    const todo = {
+        id: uuidv4(),
+        title,
+        done: false,
+        deadline: new Date(deadline),
+        created_at: new Date()
+    }
+
+    user.todos.push(todo);
+
+    return res.status(201).json(todo);
+
+});
+
+app.put("/todos/:id", checkExistsUserAccount, (req, res) => {
+    const { user } = req;
+    const { title, deadline } = req.body;
+    const { id } = req.params;
+
+    const userExistsTodo = user.todos.find(user => user.id === id);
+
+    if (!userExistsTodo) {
+        return res.status(404).json({ error: 'Todo not exists' });
+    }
+
+    userExistsTodo.title = title;
+    userExistsTodo.deadline = new Date(deadline);
+
+    return res.json(userExistsTodo);
+
+});
+
+app.patch("/todos/:id/done", checkExistsUserAccount, (req, res) => {
+    const { user } = req;
+
+    const { id } = req.params;
+
+    const userExistsTodo = user.todos.find(user => user.id === id);
+
+    if (!userExistsTodo) {
+        return res.status(404).json({ error: 'Todo not exists' });
+    }
+
+    userExistsTodo.done = true;
+
+    return res.json(userExistsTodo);
+});
+
+app.delete("/todos/:id", (req, res) => {
+    const { user } = req;
+
+    const { id } = req.params;
+
+    const todo = user.todos.findIndex(todo => todo.id === id);
+
+    if(todo == -1){
+        return res.status(404).json({ error: 'Todo not found' });
+    }
+
+    user.todos.splice(todo, 1);
+
+    return res.status(204).send();
+});
