e1f1effe317896e3b955abf6b03438c2c02b3776
rafaapcode <rafaap2003@gmail.com>
2022-04-08 17:34:22 -0300

    Post/users

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require("express");
 const app = express();
+const { v4: uuidv4 } = require("uuid");
 
 
-
-
-
+const users = [];
 
 // ROUTES
 app.post("/users", (req, res) => {
+    const { name, username } = req.body;
+
+    
+    const userExists = users.find(user => user.username == username);
+    
+    if(userExists){
+        return res.status(400).json({error: 'User already exists'});
+    }
+
+
+    const user = {
+        id: uuidv4(),
+        name: 'Danilo Vieira',
+        username: 'danilo',
+        todos: []
+    }
+
+    users.push(user);
+
+    return res.status(201).json(user);
 
 })
