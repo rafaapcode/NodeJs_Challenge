f21f5233e9d1a3c6e3361c0e1b310dea339530ac
rafaapcode <rafaap2003@gmail.com>
2022-04-08 17:53:08 -0300

    get/todos

--- index.js
+++ index.js
@@ -5,15 +5,34 @@ const { v4: uuidv4 } = require("uuid");
 
 const users = [];
 
+// Middleware
+function checkExistsUserAccount(req, res, next) {
+
+    const { username } = req.headers;
+
+    const user = users.find(user => user.username === username);
+
+    if (!user) {
+        return res.status(404).json({ error: 'User not found' });
+    }
+
+    req.user = user;
+
+    return next();
+
+}
+
+
+
 // ROUTES
 app.post("/users", (req, res) => {
     const { name, username } = req.body;
 
-    
+
     const userExists = users.find(user => user.username == username);
-    
-    if(userExists){
-        return res.status(400).json({error: 'User already exists'});
+
+    if (userExists) {
+        return res.status(400).json({ error: 'User already exists' });
     }
 
 
@@ -28,4 +47,10 @@ app.post("/users", (req, res) => {
 
     return res.status(201).json(user);
 
+});
+
+app.get("/todos", checkExistsUserAccount, (req, res) => {
+    const {user} = req;
+
+    return res.json(user.todos);
 })
